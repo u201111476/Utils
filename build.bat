7b44ea6f5f313a3fd189797d703324946c07670c
WeiJun <promise_faith@163.com>
2022-09-05 22:36:38 +0800

    修改工程配置

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-cmake.exe -S ./ -B ./build -G "Visual Studio 16 2019" -A Win32 -DPRODUCT_VERSION="\"9.9.9.9\""
+cmake.exe -S ./ -B ./build -G "Visual Studio 16 2019" -A Win32 -DMAJOR_VERSION=1
+REM  cmake.exe -S ./ -B ./build -G "Visual Studio 16 2019" -A Win32 -DMAJOR_VERSION=1 -DMINOR_VERSION=2 -DPATCH_VERSION=7
 
 cmake --build ./build --config Release
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,25 @@
 
 using namespace std;
 
-#ifndef PRODUCT_VERSION
-#define PRODUCT_VERSION "0.0.0.0"
+#ifndef MAJOR_VERSION
+#define MAJOR_VERSION   0
 #endif
+#ifndef MINOR_VERSION
+#define MINOR_VERSION   0
+#endif
+#ifndef PATCH_VERSION
+#define PATCH_VERSION   0
+#endif
+#ifndef PRODUCT_VERSION
+#define PRODUCT_VERSION "0.0.0"
+#endif
+
+#define MAKE_STRING(X) #X
+
+void foo();
 
 int main()
 {
-    cout<<"Hello world "<< PRODUCT_VERSION << "!" <<endl;
+    cout<<PRODUCT_VERSION<<endl;
     return 0;
 }
