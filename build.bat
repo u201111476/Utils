27c4465f0d8523219efb30828a95ede794e72eff
promise_faith <842645015@qq.com>
2022-09-06 16:11:47 +0800

    添加测试工程并初始化

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-cmake.exe -S ./ -B ./build -G "Visual Studio 16 2019" -A Win32 -DMAJOR_VERSION=1
-REM  cmake.exe -S ./ -B ./build -G "Visual Studio 16 2019" -A Win32 -DMAJOR_VERSION=1 -DMINOR_VERSION=2 -DPATCH_VERSION=7
+REM cmake.exe -S ./ -B ./build -G "Visual Studio 16 2019" -A Win32 -DMAJOR_VERSION=1
+cmake.exe -S ./ -B ./build -G "Visual Studio 16 2019" -A Win32 -DMAJOR_VERSION=1 -DMINOR_VERSION=2 -DPATCH_VERSION=7
 
 cmake --build ./build --config Release
--- src/Utils.h
+++ src/Utils.h
@@ -3,6 +3,8 @@
 #ifndef _WEIJUN_UTILS_H_
 #define _WEIJUN_UTILS_H_
 
+#include <Windows.h>
 
+int Base64Encode(const BYTE* inputBuf, unsigned int inputCount, char* outputBuf);
 
 #endif
